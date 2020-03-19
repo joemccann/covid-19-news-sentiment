a86cdc8d2142748f661c407d74c23ae33045f1c8
joemccann <joseph.isaac@gmail.com>
2020-03-19 10:38:48 -0700

    wip test for data

--- test/test-data.js
+++ test/test-data.js
@@ -1,18 +1,34 @@
 const test = require('tape')
 
-const main = require('../src/data')
+const { generateArticles, writeArticlesFile } = require('../src/data')
 
 test('sanity', t => {
   t.ok(true)
   t.end()
 })
 
+let content = null
+const start = 202
+const end = 499
+const filename = [String(start), '-', String(end), '.json'].join('')
+
 test('pass - build data', async t => {
-  const { err, data } = await main({ start: 202, end: 999 })
+  const { err, data } = await generateArticles({ start, end })
+  t.ok(!err)
+  t.ok(data)
+  console.log(data.length)
+  content = JSON.stringify(data)
+  t.end()
+})
+
+test('pass - write data file', async t => {
+  const { err, data } = await writeArticlesFile({
+    content,
+    filename
+  })
   t.ok(!err)
   t.ok(data)
   console.error(err)
   console.dir(data, { depth: null })
-  console.log(data.length)
   t.end()
 })
